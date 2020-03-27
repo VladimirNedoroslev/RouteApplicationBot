00a3607b3d7d870a02822e2725e8ad0133aabac3
vnedoroslev <vnedoroslev@win.tunduk.kg>
2020-03-27 10:07:58 +0600

    Removed Bot token =)

--- source/settings.py
+++ source/settings.py
@@ -12,7 +12,7 @@ INPUT_TIME_REGEX = '^([0-9]){1,2}\.([0-9]){1,2}$'
 
 API_ADDRESS = 'https://ml.tunduk.kg/Applications/Create'
 
-TELEGRAM_BOT_TOKEN = '1064566811:AAHsSkqPK7Sc4G8xUJursc099BO2YH52pdA'
+TELEGRAM_BOT_TOKEN = ''
 
 USER_DATA_REGISTRATION_FORM = 'registration_form'
 USER_DATA_APPLICATION_FORM = 'application_form'
